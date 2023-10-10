f21d99918d8d6fd4c95536af46abf4c992d2f211
yanaphat <yanaphat@sandbox.co.th>
2023-10-10 17:06:23 +0700

    first commit

---
+++ src/Facades/QrCode.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sandbox\QrCode\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Sandbox\QrCode\Generator;
+
+class QrCode extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        self::clearResolvedInstance(Generator::class);
+
+        return Generator::class;
+    }
+}
